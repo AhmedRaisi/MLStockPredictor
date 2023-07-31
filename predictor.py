14c92e346dc456831d2c59506a923031b38bcba7
alrai014 <alrai014@umn.edu>
2023-07-31 10:37:14 -0500

    Created File and Installed Libraries

---
+++ predictor.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+ 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn import metrics
+ 
+import warnings
+warnings.filterwarnings('ignore')
+
+print("No Errors")
