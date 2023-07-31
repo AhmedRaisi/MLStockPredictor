dd156ef4c0d2de100ed9f43d24dab75fa53d40b4
alrai014 <alrai014@umn.edu>
2023-07-31 13:06:05 -0500

    Started working on the predictor + Added csv file

--- predictor.py
+++ predictor.py
@@ -13,4 +13,48 @@ from sklearn import metrics
 import warnings
 warnings.filterwarnings('ignore')
 
-print("No Errors")
+
+df = pd.read_csv('TSLA.csv')
+df.head()
+print(df.shape) #Shape of the Data Set
+
+print(df.describe()) #Describe Data Set
+
+print(df.info())
+
+#Create initial Close graph to understand the closing price each day
+plt.figure(figsize=(15,5))
+plt.plot(df['Close'])
+plt.title('Tesla Close price.', fontsize=15)
+plt.ylabel('Price in dollars.')
+plt.show()
+
+
+
+print(df[df['Close'] == df['Adj Close']].shape) #Check that the close price is the same as the Adj Close
+
+#It is the same so delete redundancy 
+df = df.drop(['Adj Close'], axis=1)
+
+
+#Check for Null values
+print(df.isnull().sum()) #None exist
+
+#Graph each feature
+features = ['Open', 'High', 'Low', 'Close', 'Volume']
+ 
+plt.subplots(figsize=(20,10))
+ 
+for i, col in enumerate(features):
+  plt.subplot(2,3,i+1)
+  sb.distplot(df[col])
+plt.show()
+#We see that the graphs have two peaks except for volume
+
+#create box plot
+plt.subplots(figsize=(20,10))
+for i, col in enumerate(features):
+  plt.subplot(2,3,i+1)
+  sb.boxplot(df[col])
+plt.show()
+#We see the volume only with outliers
