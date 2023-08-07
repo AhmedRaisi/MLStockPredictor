7aed71675f07548f3f3291d86bc206124b53445f
alrai014 <alrai014@umn.edu>
2023-08-07 14:55:51 -0500

    Started backend using Flask :D

--- frontend/src/pages/ContactPage.js
+++ frontend/src/pages/ContactPage.js
@@ -7,8 +7,7 @@ const ContactPage = () => {
       <section className="hero-section">
         <h1>Contact Us</h1>
         <p>If you have any questions or feedback, please get in touch with us:</p>
-        <p>Email: support@predictorapp.com</p>
-        <p>Phone: (123) 456-7890</p>
+        <p>Email: ahmedalraisi@gmail.com</p>
       </section>
       {/* You can add a contact form or other contact information here */}
     </div>
