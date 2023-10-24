0ffa5be68dd1608d470a34bf5d74e25c680b0e9e
alrai014 <alrai014@umn.edu>
2023-10-24 15:45:36 -0500

    changed UI/UX design to be more suitable for audience

--- frontend/src/pages/PredictionPage.js
+++ frontend/src/pages/PredictionPage.js
@@ -7,18 +7,23 @@ const mockData = [
     { weekday: 'Wednesday', price: 102 },
     { weekday: 'Thursday', price: 108 },
     { weekday: 'Friday', price: 110 },
-]; // This is mock data. Replace this with your actual data.
+];
 
 const Predictions = () => {
   return (
-    <div style={{ textAlign: 'center', padding: '20px' }}>
-      <h2>Predictions for the Week</h2>
+    <div style={{ 
+        textAlign: 'center', 
+        padding: '20px',
+        backgroundColor: '#1F2833',
+        color: '#E1E5EB'
+    }}>
+      <h2 style={{ color: '#FFFFFF' }}>Predictions for the Week</h2>
       
       {/* Textual Data */}
       <ul>
         {mockData.map(data => (
-          <li key={data.weekday}>
-            {data.weekday}: Predicted Price - ${data.price}
+          <li key={data.weekday} style={{ listStyle: 'none', marginBottom: '10px' }}>
+            {data.weekday}: Predicted Closing Price - <span style={{ color: '#66FCF1' }}>${data.price}</span>
           </li>
         ))}
       </ul>
@@ -26,17 +31,21 @@ const Predictions = () => {
       {/* Chart */}
       <div style={{ maxWidth: '600px', margin: '0 auto' }}>
         <LineChart width={500} height={300} data={mockData}>
-          <Line type="monotone" dataKey="price" stroke="#8884d8" />
-          <CartesianGrid stroke="#ccc" />
-          <XAxis dataKey="weekday" />
-          <YAxis />
+          <Line type="monotone" dataKey="price" stroke="#66FCF1" />
+          <CartesianGrid stroke="#2E4159" />
+          <XAxis dataKey="weekday" stroke="#E1E5EB" />
+          <YAxis stroke="#E1E5EB" />
           <Tooltip />
           <Legend />
         </LineChart>
       </div>
 
       {/* Disclaimer */}
-      <p style={{ marginTop: '20px', color: 'red', fontWeight: 'bold' }}>
+      <p style={{ 
+          marginTop: '20px', 
+          color: '#FF5555',
+          fontWeight: 'bold'
+      }}>
         Disclaimer: These predictions are for informational purposes only and are not financial advice. Always conduct your own research before making investment decisions.
       </p>
     </div>
